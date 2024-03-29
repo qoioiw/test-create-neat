d89c46765dd7e14640e6fe78b2f2f45ff687f958
白帝子 <address>
2024-03-29 23:46:14 +0800

    测试

--- scripts/check_commit_msg.sh
+++ scripts/check_commit_msg.sh
@@ -4,21 +4,33 @@
 start_sha=$1
 end_sha=$2
 
-# 从commitlint.config.js导入rules变量
-rules=$(node -e "console.log(require('../commitlint.config.js').rules['type-enum'].join('|'))")
-
 # 设置颜色变量
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+# 使用相对路径指定文件位置
+file_path="../commitlint.config.js"
+
+# 检查文件是否存在
+if [ -f "$file_path" ]; then
+    echo "文件已找到：$file_path"
+    # 在这里执行文件相关的操作
+else
+    echo "文件未找到：$file_path"
+fi
+
+# 从commitlint.config.js导入rules变量
+rules=$(node -e "console.log(require('../commitlint.config.js').rules['type-enum'].join('|'))")
+
+echo "Rules: $rules"
+
 # 定义提交信息规范函数
 check_commit_message() {
     commit_msg="$1"
     # 检查提交信息是否以指定的前缀开头
-    # 使用外部文件的规则进行匹配检查
-    if ! echo "$commit_msg" | grep -qE "^($rules):"; then
-        echo -e "${RED}Error:${NC} Commit message format is incorrect. It should start with one of '${BLUE}$rules:${NC}'." >&2
+    if ! echo "$commit_msg" | grep -qE "$rules"; then
+        echo -e "${RED}Error:${NC} Commit message format is incorrect. It should start with one of '${BLUE}feat|fix|docs|style|refactor|test|chore|ci:${NC}'." >&2
         exit 1
     fi
 }
